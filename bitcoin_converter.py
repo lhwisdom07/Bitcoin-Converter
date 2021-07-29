817ca18030464f36c96999a9dcd4672889c9f6b1
lhwisdom07 <87719255+lhwisdom07@users.noreply.github.com>
2021-07-29 07:19:29 -0500

    Add files via upload

---
+++ bitcoin_converter.py
@@ -0,0 +1,6 @@
+print ("As of 1/13/17 at 11:13 am, bitcoin is currently trading at $39,313.80 per bitcoin.")
+bitcoinString = input("Enter the bitcoin amount: ")
+addM = float (bitcoinString)
+usD = addM * 39313.80
+
+print ( "That is worth", usD, "us dollars")
